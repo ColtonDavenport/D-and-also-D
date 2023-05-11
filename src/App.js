252c1699c8e597064357d8a21606101b1b8e5c74
Colton <coltondavenport@gmail.com>
2023-05-11 12:01:20 -0700

    connected to API, making a simple call to fetch Acid Arrow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [spell, setSpell] = React.useState({});
+
+  React.useEffect( () => {
+    (async () => {
+      try {
+        const response = await fetch("https://www.dnd5eapi.co/api/spells/acid-arrow");
+        if(!response.ok) {
+          throw Error(response.status + ": " + response.statusText);
+        }
+        const result = await response.json();
+        setSpell(result);
+      } catch (error) {
+        console.log('Fetch API Error: ' + error )
+      }
+    }) ();
+  }, []);
+
+  if(spell.index != undefined){
+    return (
+      <div> {spell.name}</div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div> Loading . . . </div>
+  )
+
 }
+
+
+// class App extends Component {
+
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+//       spell: {},
+//       isLoaded: false
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch('https://www.dnd5eapi.co/api/spells/acid-arrow')
+//       .then(res => res.json())
+//       .then(json => {
+//         this.setState = {
+//           isLoaded: true,
+//           spell: json
+//         }
+//         console.log(this.state.isLoaded);
+//       });
+//   }
+
+//   render() {
+
+//     let {isLoaded, spell} = this.state;
+//     if (!isLoaded) return (
+//       <div className="App">
+//         waiting to load
+//       </div>
+//     )
+
+//     return (
+//       <div className="App">
+//         loaded
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
